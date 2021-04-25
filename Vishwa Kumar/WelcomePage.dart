462c053324ab376df5c99a75c3490818b3e86c73
vishwaaa-kumar <72905310+vishwaaa-kumar@users.noreply.github.com>
2021-04-25 14:19:27 +0530

    Update WelcomePage.dart

--- Vishwa Kumar/WelcomePage.dart
+++ Vishwa Kumar/WelcomePage.dart
@@ -1,33 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'MyApp.dart';
+import './body.dart';
 
-class WelcomePage extends StatelessWidget {
-  void selectCategory(BuildContext ctx) {
-    Navigator.of(ctx).push(
-      MaterialPageRoute(
-        builder: (_) {
-          return MyApp();
-        },
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Optimal Character Recognizer"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => selectCategory(context),
-        child: Icon(Icons.arrow_forward_outlined),
-      ),
-      body: Container(
-        child: Center(
-          child: Text("Head to Image Upload and Preview Page"),
-        ),
-      ),
-    );
+class WelcomePage extends StatelessWidget
+{
+  @override 
+  Widget build(BuildContext context)
+  {
+    return Scaffold(body: Body(),);
   }
 }
